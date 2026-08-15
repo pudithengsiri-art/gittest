c9e6ca02eaf6ec0a62e442da60197ee672496d09
Pudit Hengsiri <pudit.hengsiri@gmail.com>
2026-08-15 16:10:41 +0700

    second commit

---
+++ work.sh
@@ -0,0 +1,29 @@
+echo "--------------------"
+echo "Enter first number"
+read num1
+echo "Enter second number"
+read num2
+echo "Enter third number"
+read num3
+min=$num1
+max=$num1
+if [ $num2 -lt $min ]
+then
+    min=$num2
+fi
+if [ $num3 -lt $min ]
+then
+    min=$num3
+fi
+if [ $num2 -gt $max ]
+then
+    max=$num2
+fi
+if [ $num3 -gt $max ]
+then
+    max=$num3
+fi
+echo "--------------------"
+echo "Minimum number is: $min"
+echo "Maximum number is: $max"
+echo "--------------------"
